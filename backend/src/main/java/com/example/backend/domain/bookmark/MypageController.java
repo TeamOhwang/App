43dc42ef5206413271e2f3983b71e7c00409a83f
곽희돈 <khdon0405789@gmail.com>
2025-08-06 10:49:52 +0900

    fix : mypage 수정

--- backend/src/main/java/com/example/backend/domain/bookmark/MypageController.java
+++ backend/src/main/java/com/example/backend/domain/bookmark/MypageController.java
@@ -1,42 +1,53 @@
-//package com.example.backend.domain.bookmark;
-//
-//import java.util.List;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PatchMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.example.backend.domain.post.Post;
-//
-//import lombok.RequiredArgsConstructor;
-//
-//@RestController
-//@RequestMapping("/api/mypage")
-//@RequiredArgsConstructor
-//public class MypageController {
-//        private final MypageService mypageService;
-//
-//    @GetMapping("/profile/{userId}") // 회원 정보 조회
-//    public ResponseEntity<UserProfileResponseDto> getMyProfile(@PathVariable Long userId) {
-//        UserProfileResponseDto profile = mypageService.getMyProfile(userId);
-//        return ResponseEntity.ok(profile);
-//    }
-//
-//@PatchMapping("/mypage/nickname") // 닉네임 수정
-//public ResponseEntity<Void> updateNickname(@RequestParam String nickname, @RequestParam Long userId) {
-//    mypageService.updateMypage(userId, nickname);
-//    return ResponseEntity.ok().build();
-//}
-//
-//@GetMapping("/mypage/posts/{userId}") // 내 게시글 불러오기
-//public ResponseEntity<List<Post>> getMyPosts(@PathVariable Long userId) {
-//    return ResponseEntity.ok(mypageService.myPostsLoad(userId));
-//}
-//  
-//
-//    
-//// }
+package com.example.backend.domain.bookmark;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.domain.bookmark.DTO.PostWithLikeCountDto;
+import com.example.backend.domain.bookmark.DTO.UserProfileDto;
+import com.example.backend.domain.post.Post;
+import com.example.backend.entity.Users;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/mypage")
+@RequiredArgsConstructor
+public class MypageController {
+        private final MypageService mypageService;
+
+@GetMapping("/profile/{userId}")//내 프로필 불러오기
+public ResponseEntity<UserProfileDto> getMyProfile(@PathVariable Long userId) {
+    Users user = mypageService.getMyProfileEntity(userId);
+    return ResponseEntity.ok(new UserProfileDto(user));
+}
+
+@PatchMapping("/mypage/nickname")//닉네임 수정
+public ResponseEntity<Void> updateNickname( @RequestParam String nickname, @RequestParam Long userId) {
+    mypageService.updateMypage(userId, nickname);
+    return ResponseEntity.ok().build();
+}
+
+
+@GetMapping("/mypage/posts-with-likes/{userId}") // 내 게시글과 좋아요 수 불러오기
+public ResponseEntity<List<PostWithLikeCountDto>> getMyPostsWithLikeCount(@PathVariable Long userId) {
+    return ResponseEntity.ok(mypageService.myPostsWithLikeCount(userId));
+}
+
+
+@GetMapping("/mypage/likes/{userId}") // 좋아요한 게시글 불러오기
+public ResponseEntity<List<Post>> getLikedPosts(@PathVariable Long userId) {
+    return ResponseEntity.ok(mypageService.getLikedPosts(userId));
+}
+
+
+
+}
+
