b27224186f8fd4866abcfc2f5f103b36fa8f48fe
이광원 <leekw0228@gmail.com>
2025-08-07 11:41:27 +0900

    어플 실행 Splash 화면
    어플 실행 Splash 화면

---
+++ android/app/src/main/java/com/example/project/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.project
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    // 스플래시 화면이 보여지는 시간 (2초)
+    private val SPLASH_TIME_OUT: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Handler를 사용하여 일정 시간 후에 MainActivity로 이동
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // SplashActivity를 종료하여 뒤로가기 버튼으로 돌아오지 않도록 함
+        }, SPLASH_TIME_OUT)
+    }
+}
